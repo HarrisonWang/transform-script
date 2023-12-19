90b12fd81e0941ff5c4077ab0330c6572c790791
Harrison Wang <harrisonwang@163.com>
2023-12-19 12:53:08 +0800

    新增测试用例

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+// __tests__/math.test.js
+
+const { add } = require('../math');
+
+test('add function adds two numbers', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('add function adds negative numbers', () => {
+  expect(add(-1, -2)).toBe(-3);
+});
+
+test('add function adds floating point numbers', () => {
+  expect(add(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
